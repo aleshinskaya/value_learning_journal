ad8fe2c41ea90af4b423d20187e84c009b95a76a
aleshinskaya <anna.leshinskaya@gmail.com>
2024-03-14 01:20:35 -0700

    improved the code

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # value_learning_journal
+
+
+Use wrapper.py to compare prompts! 
+
+Save entries in data/
+
+Outputs will be stored in the same directory.
+
+Use existing entries to explore.
--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import os
 from dotenv import dotenv_values
 from dotenv import load_dotenv
-import json,jsonlines
+import json
 import requests
-import re, io, ast
-import pandas as pd
-import numpy as np
+# import re, io, ast
+# import pandas as pd
+# import numpy as np
 import random
 import importlib
 import prompts
@@ -18,26 +18,56 @@ global config
 config = dotenv_values(".env")
 CUR_DIR = os.path.dirname(os.path.abspath(__name__))
 
-# overview:
-# input is a jounral entry and person's name
-# output is a set of extracted values 
 
-# prompts are stored in .py files in this directory
 
-# list of prompts to test
-PROMPTS_TO_TEST = ['prompts.PROMPT_1','prompts.PROMPT_2','prompts.PROMPT_3']
-
-journal_filename = '/Users/anna/Dropbox/AOI/MoralLearning/CodeSets/value_learning_journal/data/text-e1.txt'
-
-# provide full path here 
-def main(journal_filename: str, person_name: str = 'user'):   
+def test_two_step(journal_filename: str, person_name: str = 'user', prompt_list: list= []):   
   """
   journal-filename: a full path to a single text file containing a journal entry
   person-name: optional, defaults to 'user'.
+  prompt_list: a list of system prompt instructions that will be varied
   """
+  split_fn = journal_filename.split('.')
+  
+  #read in the file
+  try:
+    with open(journal_filename, 'r') as file:
+      this_entry = file.read()
+  except:
+      raise FileExistsError()
+
+  n_prompt = 0
+  #pass the entry to the selected prompt
+  for this_p in prompt_list: 
+
+    # call prompts with entry and this prompt to generate prompt text
+    prompt_text = prompts.PROMPT_1(this_entry,this_p)
+    # use this prompt to query GPT
+    prompt_response = promptGPT_json(prompt_text,0.7)  
+    # save response
+    response_list=json.loads(prompt_response)  
+    print(response_list)
+    #pass to a second stage where these items get scored 
+    next_prompt = prompts.prompt_rate(response_list["values"],this_entry)
+    prompt_response = promptGPT_json(next_prompt,0.7)  
+    # save final output
+    this_response = json.loads(prompt_response)
+    # outputs_dict = this_response["items"]
+    # print(this_response)
+    # save to json file
+    outfile_name = split_fn[0]+ '_two_step_prompt_'+str(n_prompt)+'.json'      
+    #output a datastructure wth the items and ratings
+    write_json(outfile_name,this_response,this_p)
+    n_prompt = n_prompt+1
 
 
-  outfile_name = journal_filename+'.json'
+def test_one_step(journal_filename: str, person_name: str = 'user', prompt_list: list= []):   
+  """
+  journal-filename: a full path to a single text file containing a journal entry
+  person-name: optional, defaults to 'user'.
+  prompt_list: a list of system prompt instructions that will be varied
+  """
+  split_fn = journal_filename.split('.')
+  outfile_name = split_fn[0]+ '_one_step'+'.json'
 
   #read in the file
   try:
@@ -46,22 +76,23 @@ def main(journal_filename: str, person_name: str = 'user'):
   except:
       raise FileExistsError()
 
-
+  n_prompt = 0
   #pass the entry to the selected prompt
-  for this_p in PROMPTS_TO_TEST:      
-    # this_p = PROMPTS_TO_TEST[2]  
-    this_prompt_function  = eval(this_p)
-    prompt_text = this_prompt_function(this_entry)
+  for this_p in prompt_list: 
+
+    # call prompts with entry and this prompt to generate prompt text
+    prompt_text = prompts.PROMPT_1(this_entry,this_p)
+    # use this prompt to query GPT
     prompt_response = promptGPT_json(prompt_text,0.7)  
-    response_list=json.loads(prompt_response)
-  
-    #pass to a second stage where these items get scored 
-    next_prompt = prompt_rate(response_list["values"],this_entry)
-    prompt_response = promptGPT_json(next_prompt,0.7)  
-    response_dict = json.loads(prompt_response)
-    
+    # print(prompt_response)
+    # save response
+    response_list=json.loads(prompt_response)  
+    outfile_name = split_fn[0]+ '_one_step_prompt_'+str(n_prompt)+'.json'      
     #output a datastructure wth the items and ratings
-    write_json(outfile_name,response_dict["items"])
+    write_json(outfile_name,response_list,this_p)
+    n_prompt = n_prompt+1
+
+    
 
   
 
@@ -112,15 +143,17 @@ def promptGPT4(prompt_message_list, gpt_temperature=0,debug=False):
 
   return output
 
-def write_json(fname,jlist):
-    jsonl_file =  open(fname, 'w')  
-    for dictionary in jlist:
-        jsonl_file.write(json.dumps(dictionary) + '\n')
+def write_json(fname,jlist,prompt):
+    
+    jsonl_file =  open(fname, 'w') 
+
+    d = {'prompt': prompt}
+    jsonl_file.write(json.dumps(d)+ '\n')
+
+    for dictionary in jlist['values']:
+      jsonl_file.write(json.dumps(dictionary) + '\n')
 
     jsonl_file.close()
-
-if __name__ == "__main__":
-    typer.run(main)
 
 
 
